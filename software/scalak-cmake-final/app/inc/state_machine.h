3c9795dc70af0e1b8a06f8d3b6e26020e3196cfa
Eryk Możdżeń <ermozdzen@gmail.com>
2022-10-29 16:02:04 +0200

    state machine cleaning

--- software/scalak-cmake-final/app/inc/state_machine.h
+++ software/scalak-cmake-final/app/inc/state_machine.h
@@ -1,15 +1,7 @@
-/**
- * @file finite_state_machine.h
- * @author Eryk Możdżeń
- * @date 2021-12-24
- */
-
 #ifndef STATE_MACHINE_H
 #define STATE_MACHINE_H
 
-#include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
 
 #define FSM_STATE_MAX_NUM	10
 #define FSM_EVENT_MAX_NUM	4
@@ -19,39 +11,29 @@ typedef uint8_t (*__fsm_getter_t)();
 
 struct __fsm_state;
 
-/**
- * @brief event object
- */
 struct __fsm_event {
-	__fsm_getter_t get;				/*< function to check if event occurs, set NULL if no event is needed to switch state */
-    struct __fsm_state *next_state;	/*< pointer to next state */
+	__fsm_getter_t get;
+    struct __fsm_state *next_state;
 };
 
-/**
- * @brief state object
- */
 struct __fsm_state {
-	uint8_t id;                						/*< user identifier of specific state, must be uniq */
+	uint8_t id;
 
-	__fsm_behavior_t enter;      					/*< function called once, at the beginning of the state, after switch from previous one, set NULL if not needed */
-	__fsm_behavior_t execute;    					/*< function called every time when the state is used, set NULL if not needed  */
-	__fsm_behavior_t exit;       					/*< function called once, at the end of the state, before switch to the next one, set NULL if not needed  */
+	__fsm_behavior_t enter;
+	__fsm_behavior_t execute;
+	__fsm_behavior_t exit;
 
-	struct __fsm_event events[FSM_EVENT_MAX_NUM];	/*< set of defined events for this state */
-	uint8_t events_num;								/*< number of defined events */
+	struct __fsm_event events[FSM_EVENT_MAX_NUM];
+	uint8_t events_num;
 };
 
-/**
- * @brief state machine object structure
- */
 typedef struct {
-	struct __fsm_state *curr_state;					/*< pointer to current state */
-    struct __fsm_state states[FSM_STATE_MAX_NUM];	/*< indexed set of defined states */
-	uint8_t states_num;								/*< number of defined states */
+	struct __fsm_state *curr_state;
+    struct __fsm_state states[FSM_STATE_MAX_NUM];
+	uint8_t states_num;
 } fsm_t;
 
 void fsm_init(fsm_t *);
-
 void fsm_define_state(fsm_t *, uint8_t, __fsm_behavior_t, __fsm_behavior_t, __fsm_behavior_t);
 void fsm_define_transition(fsm_t *, uint8_t, uint8_t, __fsm_getter_t);
 
--- software/scalak-cmake-final/app/src/state_machine.c
+++ software/scalak-cmake-final/app/src/state_machine.c
@@ -1,17 +1,6 @@
-/**
- * @file finite_state_machine.c
- * @author Eryk Możdżeń
- * @date 2021-12-24
- */
-
 #include "state_machine.h"
+#include <stddef.h>
 
-/**
- * @brief Search for pointer to specific state.
- * @param st pointer to state machine
- * @param id identifier of the state
- * @return pointer to state, NULL if not defined
- */
 static struct __fsm_state * get_state_ptr(fsm_t *st, uint8_t id) {
     for(uint8_t i=0; i<st->states_num; i++) {
         if(st->states[i].id==id) {
@@ -22,98 +11,61 @@ static struct __fsm_state * get_state_ptr(fsm_t *st, uint8_t id) {
     return NULL;
 }
 
-/**
- * @brief Initialize fields in state machine structure
- * @param st pointer to state machine
- */
 void fsm_init(fsm_t *st) {
-    // init fields
     st->curr_state = NULL;
 	st->states_num = 0;
 }
 
-/**
- * @brief Add state definition to state machine
- * @param st pointer to state machine
- * @param id state identifier
- * @param enter state enter function
- * @param execute state execute function
- * @param exit state exit function
- */
 void fsm_define_state(fsm_t *st, uint8_t id, __fsm_behavior_t enter, __fsm_behavior_t execute, __fsm_behavior_t exit) {
 
 	if(st->states_num>=FSM_STATE_MAX_NUM)
 		return;
 
-    // write init values
     st->states[st->states_num].id = id;
     st->states[st->states_num].enter = enter;
     st->states[st->states_num].execute = execute;
     st->states[st->states_num].exit = exit;
 	st->states[st->states_num].events_num = 0;
-
 	st->states_num++;
 }
 
-/**
- * @brief Set transition between two states caused by presents of specific event
- * @param st pointer to state machine
- * @param curr_id identifier of the state when event occurred
- * @param next_id identifier of the state that should be set after event occurred
- * @param get event getter function
- */
 void fsm_define_transition(fsm_t *st, uint8_t curr_id, uint8_t next_id, __fsm_getter_t get) {
 
-    // get states
 	struct __fsm_state *curr_state = get_state_ptr(st, curr_id);
 	struct __fsm_state *next_state = get_state_ptr(st, next_id);
+
 	if(!curr_state || !next_state)
 		return;
 
-    // write data
 	if(curr_state->events_num>=FSM_EVENT_MAX_NUM)
 		return;
 
     curr_state->events[curr_state->events_num].get = get;
     curr_state->events[curr_state->events_num].next_state = next_state;
-	
 	curr_state->events_num++;
 }
 
-/**
- * @brief Set inital state for state machine, call enter function if exist
- * @param st pointer to state machine
- * @param initial_id identifier of state that should be at the begin of the program, must be defined before
- */
 void fsm_start(fsm_t *st, uint8_t initial_id) {
 
 	struct __fsm_state *initial_state = get_state_ptr(st, initial_id);
+
     if(!initial_state)
         return;
 
-	// set current state to initial state
 	st->curr_state = initial_state;
 
-    // call enter function for current state
     if(st->curr_state->enter)
         st->curr_state->enter();
 }
 
-/**
- * @brief Check if any event occurred, change state if it's required
- * @param st pointer to state machine
- */
 void fsm_update(fsm_t *st) {
 
 	if(!st->curr_state)
 		return;
 
-    // check if any event defined for current state occurs
 	const struct __fsm_event * event = NULL;
 
     for(uint8_t i=0; i<st->curr_state->events_num; i++) {
-    	// check if event occurs
-    	// if event getter is NULL, no event is needed to switch state
 
     	if(st->curr_state->events[i].get) {
     		if(st->curr_state->events[i].get())
@@ -126,27 +78,20 @@ void fsm_update(fsm_t *st) {
 	if(!event)
 		return;
 
-	// call exit function for current state if exist
 	if(st->curr_state->exit)
 		st->curr_state->exit();
-	
-	// change current state
+
 	st->curr_state = event->next_state;
 
-	// call enter function for next state if exist
 	if(st->curr_state->enter)
 		st->curr_state->enter();
 }
 
-/**
- * @brief Call execute function for current state
- * @param st pointer to state machine
- */
 void fsm_execute(fsm_t *st) {
+
 	if(!st->curr_state)
 		return;
 
-	// call execute function for current state if exist
 	if(st->curr_state->execute)
 		st->curr_state->execute();
 }
